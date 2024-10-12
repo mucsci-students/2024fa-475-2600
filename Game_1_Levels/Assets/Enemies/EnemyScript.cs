c302c1134ad15741861caec0cdbe13530eec0e35
John-Hershey <jjhersh1@millersville.edu>
2024-10-12 00:27:58 -0400

    green slime anim and base enemy script

--- Game_1_Levels/Assets/Enemies/EnemyScript.cs
+++ Game_1_Levels/Assets/Enemies/EnemyScript.cs
@@ -4,31 +4,90 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // declared components
     private Animator anim;
-    private bool facingLeft = true;
-
-    [SerializeField] private float health;
+    private Rigidbody2D body;
+    private Transform targetPoint;
+    // field values
+    [SerializeField] private float health = 40;
+    [SerializeField] private float damage = 10;
+    // linked objects
+    public HealthManager script;
+    public GameObject pointA;
+    public GameObject pointB;
+    //internal variables
+    public float speed = 1;
+    public bool paused = false;
 
     void Start()
     {
         anim = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+        targetPoint = pointB.transform;
+        anim.SetBool("isRunning", true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Time.timeScale == 0 || paused)
+        {
+            return;
+        }
+        int negative = -1;
+        if (targetPoint == pointB.transform)
+        {
+            negative *=-1;
+        }
+        body.velocity = new Vector2(speed*negative, body.velocity.y);
         
+        if (Mathf.Abs(transform.position.x - targetPoint.position.x) < 0.5f)
+        {
+            Debug.Log("I should flip now");
+            flip();
+            anim.SetBool("isRunning", false);
+            StartCoroutine(goIdle());
+            anim.SetBool("isRunning", true);
+            if(targetPoint == pointB)
+            {
+                targetPoint = pointA.transform;
+            }
+            else
+            {
+                targetPoint = pointB.transform;
+            }
+        }
     }
+
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    IEnumerator goIdle() 
+    {
+        paused = true;
+        yield return new WaitForSeconds (2f);
+        paused = false;
+    }
+    
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "sword")
+        if (other.tag == "Sword")
         {
             health -=10;
             if(health <=0 ){
-                anim.SetTrigger("hurt");
+                anim.SetTrigger("death");
+                Destroy(transform.parent.gameObject, 0.5f);
             }
-            
+            else{
+                anim.SetTrigger("hurt");
+            } 
+        }
+        else if(other.tag == "Player"){
+            script.takeDamage(damage);
         }
     }
 }
--- Game_1_Levels/Assets/Enemies/GreenSlimeAnimation.cs
+++ Game_1_Levels/Assets/Enemies/GreenSlimeAnimation.cs
@@ -8,44 +8,20 @@ public class GreenSlimeAnimation : MonoBehaviour
     private float timer = 0f;
     private Animator anim;
     private Rigidbody2D body;
-
-    [SerializeField] Vector2 jumpDistance;
-    [SerializeField] public float health;
-    // Start is called before the first frame update
+    [SerializeField] public float jumpHeight;
     void Start()
     {
-        
         anim = GetComponent<Animator>();
         body = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-       
         timer += Time.deltaTime;
-        if(timer > 2)
+        if(timer > 4 && anim.GetBool("isRunning"))
         {
             timer = 0;
             anim.SetTrigger("jump");
-            body.velocity = jumpDistance;
-        }
-
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Sword")
-        {
-            health -= 10;
-            if(health <= 0)
-            {
-                anim.SetTrigger("death");
-            }
-            else
-            {
-                anim.SetTrigger("hurt");
-            }
+            body.velocity = new Vector2(body.velocity.x, jumpHeight);
         }
     }
 }
