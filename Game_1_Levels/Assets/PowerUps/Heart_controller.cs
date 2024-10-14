d11ad76e5ffd846f5b7c2db6921267c28460309c
Kyle <nonplayercontrolled@gmail.com>
2024-10-13 20:28:48 -0400

    Created Gray and red heart prefab see powerups folder

---
+++ Game_1_Levels/Assets/PowerUps/Heart_controller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart_controller : MonoBehaviour
+{
+    private GameObject healthPack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthPack = gameObject;
+    }
+
+     void OnTriggerEnter2D (Collider2D other)
+    {
+        if(other.tag == "Player"){
+        // add your health or other desired functionality here
+        Destroy(healthPack);
+        print("Health pack Destroyed");
+        }
+    }
+}
