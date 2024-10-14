a76a9be7e1433105c98eac4423a2deb95175650d
kdicher <kevinidichter@gmail.com>
2024-10-14 18:26:31 -0400

    made changes to pause screen and respawn screen. Made audio sliders work

---
+++ Game_1_Levels/Assets/Scripts/SoundMixerManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundMixerManager : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+
+    public void SetSoundFXVolume(float level)
+    {
+        audioMixer.SetFloat("soundFXVolume", Mathf.Log10(level) * 20f);
+    }
+
+    public void SetMusicVolume(float level)
+    {
+        audioMixer.SetFloat("musicVolume", Mathf.Log10(level) * 20f);
+    }
+}
