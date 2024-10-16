0c548975ea0f68113c1fe166b26bb490b1a720c4
John-Hershey <jjhersh1@millersville.edu>
2024-10-16 18:56:52 -0400

    level complete screen

--- Game_1_Levels/Assets/Scripts/Boss Scripts/LVL2BossHealth.cs
+++ Game_1_Levels/Assets/Scripts/Boss Scripts/LVL2BossHealth.cs
@@ -7,12 +7,27 @@ public class LVL2BossHealth : MonoBehaviour
 {
     public Image bossHealthBar;
     public SkeletonMovement script;
+    private GameObject screen;
 
+    void Start()
+    {
+        screen = GameObject.Find("WinScreen");
+        screen.SetActive(false);
+    }
+    public void Win()
+    {
+        Time.timeScale = 0;
+        PlayerPrefs.SetInt("tempspawn", 3);
+        screen.SetActive(true);
+    }
     void OnTriggerEnter2D (Collider2D other)
     {
-        if (other.tag == "Sword")
+        if (other.tag == "Sword" || other.tag == "PowerUp")
         {
             bossHealthBar.fillAmount = script.health / 750f;
+            if(script.health < 20){
+                Win();
+            }
         }
     }
 }
