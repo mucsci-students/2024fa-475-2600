98de2307f115ec1044a6d0bc2d74db6b4c340043
John-Hershey <jjhersh1@millersville.edu>
2024-10-15 23:53:01 -0400

    removing unused enemy assets

---
+++ Game_1_Levels/Assets/Enemies/PSBSkeleton/SkeletonMovement.cs
@@ -0,0 +1,171 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SkeletonMovement : MonoBehaviour
+{
+    // declared components
+    private Animator anim;
+    private Rigidbody2D body;
+    private Transform currentPoint;
+    private SpriteRenderer rend;
+    // field values
+    public float health = 80f;
+
+    private bool myTargetValid;
+    //sound effects
+    [SerializeField] private AudioClip attackSound;
+    [SerializeField] private AudioClip hurtSound;
+    [SerializeField] private AudioClip deathSound;
+    // linked objects
+    public HealthManager script;
+    public GameObject pointA;
+    public GameObject pointB;
+    public GameObject heartPrefab;
+    //internal variables
+    public float speed = 1;
+
+    private float runTimer = 0f;
+    private bool canRun;
+    public bool isDead = false;
+
+    public GameObject skeletonChild;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+        currentPoint = pointA.transform;
+        anim.SetTrigger("MoveTrigger");
+        myTargetValid = true;
+        canRun = true;
+        rend = this.gameObject.GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+        runTimer += Time.deltaTime;
+        if (runTimer >= 1.33f)
+        {
+            if (canRun)
+            {
+                anim.SetTrigger("MoveTrigger");
+            }
+            runTimer = 0f;
+        }
+        if(Time.timeScale == 0)
+        {
+            return;
+        }
+        Vector2 point = currentPoint.position - transform.position;
+        if (currentPoint == pointB.transform && myTargetValid)
+        {
+            body.velocity = new Vector2(speed, 0);
+        }
+        else if (currentPoint == pointA.transform && myTargetValid)
+        {
+            body.velocity = new Vector2(-speed, 0);
+        }
+        
+        if (Mathf.Abs(transform.position.x - currentPoint.position.x) < .8f)
+        {
+            flip();
+            if(currentPoint == pointB.transform)
+            {
+                currentPoint = pointA.transform;
+            }
+            else
+            {
+                currentPoint = pointB.transform;
+            }
+        }
+        healthCheck();
+    }
+    private void flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+    void randomHeart()
+    {
+        int random = Random.Range(0, 10); // 0 - 9
+        Transform temp = this.transform;
+        if (random < 3)
+        {
+            Instantiate(heartPrefab, temp.position, Quaternion.identity);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Sword" && isDead == false)
+        {
+            health -=10;
+            if(health <=0)
+            {
+                SoundFXManager.instance.PlaySoundFXClip(deathSound, transform, 0.25f);
+                anim.SetTrigger("DeathTrigger");
+            }
+            else
+            {
+                SoundFXManager.instance.PlaySoundFXClip(hurtSound, transform, .2f);
+                StartCoroutine(takeDamage());
+            } 
+        }
+    }
+    private void healthCheck()
+    {
+        if(health <= 0)
+        { 
+            myTargetValid = false;
+            canRun = false;
+            isDead = true;
+            StartCoroutine(death());
+        }
+    }
+    public void startAttack()
+    {
+        //anim.SetBool("isRunning", false);
+        healthCheck();
+        canRun = false;
+        myTargetValid = false;
+        anim.SetTrigger("AttackTrigger");
+        SoundFXManager.instance.PlaySoundFXClip(attackSound, transform, .2f);
+        StartCoroutine(attack());
+    }
+
+    IEnumerator attack()
+    {
+        healthCheck();
+        yield return new WaitForSeconds (.5f);
+        myTargetValid = true;
+        canRun = true;
+    }
+    IEnumerator takeDamage()
+    {
+        skeletonChild.SetActive(false);
+        yield return new WaitForSeconds (.1f);
+        skeletonChild.SetActive(true);
+        yield return new WaitForSeconds (.1f);
+        skeletonChild.SetActive(false);
+        yield return new WaitForSeconds (.1f);
+        skeletonChild.SetActive(true);
+    }
+
+    IEnumerator death()
+    {
+        yield return new WaitForSeconds (1.5f);
+        //SoundFXManager.instance.PlaySoundFXClip(deathSound, transform, 0.1f);
+        Destroy(this.gameObject);
+        randomHeart();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.8f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.8f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
+    }
+
+}
+
